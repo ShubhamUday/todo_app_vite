9b90bc91ebde263771b579a8dc4c67956c7f973c
shubhamuday <shubhamuday21@gmail.com>
2025-05-31 16:57:52 +0530

    backend deploy fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://leafy-gnome-cb85aa.netlify.app/",
+      "https://leafy-gnome-cb85aa.netlify.app",
     ], // your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
